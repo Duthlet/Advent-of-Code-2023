fac4c6d68ca0c2f022e78a511501ba856c2ec8d2
Duthlet <peacefulwesnoth@gmail.com>
2023-12-07 09:24:59 +0100

    Day 7: Make solution cleaner

--- day_07/main.cpp
+++ day_07/main.cpp
@@ -6,28 +6,21 @@
 #include <shortinttypes.h>
 
 struct Hand {
-
-    std::string uoh;
+    std::string hand;
     u32 bid;
-
     int type;
 
-    void calc_type(bool part2) {
-        std::map<char, int> m {};
-        for (auto c : uoh) {
-            if (m.find(c) == m.end()) {
-                m[c] = 1;
-            } else {
-                m[c] += 1;
-            }
-        }
+    Hand (std::string s, u32 i) : hand(s), bid(i) {}
 
-        int nuj = 0;
+    void calc_type(bool part2) {
+
+        std::map<char, int> m {};
+        for (auto c : hand) ++m[c];
+
+        int num_jokers = 0;
         if (part2) {
-            if (m.find('J') != m.end()) {
-                nuj = m['J'];
-                m['J'] = 0;
-            }
+            num_jokers = m['J'];
+            m.erase('J');
         }
 
         std::vector<int> v;
@@ -37,83 +30,72 @@ struct Hand {
         std::sort (v.begin(), v.end());
 
         if (part2) {
-            if (v.size() != 0) {
-                *(v.end()-1) += nuj;
-            } else {
-                v.push_back(nuj);
-            }
+            if (v.empty()) v.push_back(0);
+            v.back() += num_jokers;
         }
 
-        if (v == std::vector{1, 1, 1, 1, 1})
-            type = 0;
-        else if (v == std::vector{1, 1, 1, 2})
-            type = 1;
-        else if (v == std::vector{1, 2, 2})
-            type = 2;
-        else if (v == std::vector{1, 1, 3})
-            type = 3;
-        else if (v == std::vector{2, 3})
-            type = 4;
-        else if (v == std::vector{1, 4})
-            type = 5;
-        else if (v == std::vector{5})
-            type = 6;
-        else {
-            throw(1);
-        }
+        type =
+            (std::map<std::vector<int>, int> {
+            {{1, 1, 1, 1, 1}, 0},
+            {{1, 1, 1, 2}, 1},
+            {{1, 2, 2}, 2},
+            {{1, 1, 3}, 3},
+            {{2, 3}, 4},
+            {{1, 4}, 5},
+            {{5}, 6},
+            }).at(v);
     }
-
-    Hand (std::string s, u32 i) : uoh(s), bid(i) {
-        calc_type(false);
-    }
-
-
 };
 
-
-int cmpcard (char a, char b, std::vector<char> &card_values) {
+int cmpcard (char a, char b, const std::vector<char> &card_values) {
     auto it_a = std::find(card_values.begin(), card_values.end(), a);
     auto it_b = std::find(card_values.begin(), card_values.end(), b);
-    if (it_a < it_b) return 1;
-    if (it_a > it_b) return -1;
+    if (it_a < it_b) return -1;
+    if (it_a > it_b) return 1;
     return 0;
 }
 
-bool cmp (const Hand& a, const Hand& b, std::vector<char> &card_values) {
-    if(a.type > b.type) return true;
-    if(a.type < b.type) return false;
+bool cmp (const Hand& a, const Hand& b, const std::vector<char> &card_values) {
+    if(a.type < b.type) return true;
+    if(a.type > b.type) return false;
     for(int i = 0; i < 5; ++i) {
-        auto cc = cmpcard(a.uoh[i], b.uoh[i], card_values);
-        if (cc == 1) return true;
-        if (cc == -1) return false;
+        switch (cmpcard(a.hand[i], b.hand[i], card_values)) {
+            case 1 : return true;
+            case -1: return false;
+        }
     }
-    throw(2);
+    return false;
 }
 
 u64 sum (const std::vector<Hand> &hands) {
     u64 sum = 0;
-    unsigned int i = 1;
-    for (auto it = hands.rbegin(); it != hands.rend(); ++it) {
-        sum += i * it->bid;
-        ++i;
+    unsigned int rank = 1;
+    for (const auto& hand : hands) {
+        sum += rank * hand.bid;
+        ++rank;
     }
     return sum;
 }
 
 #include "input.inc"
 
+void sort_input(const std::vector<char> &card_values) {
+    std::sort(
+        input.begin(),
+        input.end(),
+        [&](const Hand& a, const Hand& b){return cmp(a,b,card_values);}
+    );
+}
+
 int part1() {
-    std::vector<char> card_values
-    {'A','K','Q','J','T','9','8','7','6','5','4','3','2'};
-    std::sort(input.begin(), input.end(), [&](const Hand& a, const Hand& b){return cmp(a,b,card_values);});
+    for (auto &h : input) h.calc_type(false);
+    sort_input({'A','K','Q','J','T','9','8','7','6','5','4','3','2'});
     return sum(input);
 }
 
 int part2() {
     for (auto &h : input) h.calc_type(true);
-    std::vector<char> card_values
-    {'A','K','Q','T','9','8','7','6','5','4','3','2','J'};
-    std::sort(input.begin(), input.end(), [&](const Hand& a, const Hand& b){return cmp(a,b,card_values);});
+    sort_input({'A','K','Q','T','9','8','7','6','5','4','3','2','J'});
     return sum(input);
 }
 
